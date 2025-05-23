8ea127c1e50854894afda623999eebf8925e4048
DanyFadhilah <fadilahdany@gmail.com>
2025-05-23 07:26:19 +0700

    Responsive Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,13 +51,15 @@ export default function HomePage() {
     <>
       <Navbar />
       <div className="bg-black p-3">
-        <div className="bg-[url('/bg.png')] h-[700px] bg-no-repeat bg-cover rounded-2xl flex gap-10 justify-between px-38 items-center">
-          <div className="flex flex-col gap-5">
-            <p className="text-6xl font-bold w-3/4">Invest for your future</p>
-            <p className="text-xl text-gray-400 font-medium w-3/4">
+        <div className="bg-[url('/bg.png')] h-[700px] bg-no-repeat bg-cover rounded-2xl flex gap-10 justify-between sm:px-30 sm:p-30 p-10 items-center">
+          <div className="flex flex-col gap-5 items-center text-center sm:items-center lg:items-start sm:text-center lg:text-left">
+            <p className="text-5xl sm:text-6xl font-bold w-3/4">
+              Invest for your future
+            </p>
+            <p className="text-md sm:text-xl text-gray-400 font-medium w-3/4">
               Trusted by millions to buy, sell, and manage crypto seamlessly.
             </p>
-            <div className="flex gap-5">
+            <div className="flex gap-5 flex-col items-center sm:flex-col md:flex-row">
               <div className="relative">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-5">
                   <EnvelopeIcon className="h-5 w-5 text-white" />
@@ -66,7 +68,7 @@ export default function HomePage() {
                   type="email"
                   id="email"
                   placeholder="satoshi@nakamoto.com"
-                  className="w-[380px] pl-13 pr-3 py-3 h-[56px] placeholder-gray-300 border border-[#717387] rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[linear-gradient(to_bottom,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]"
+                  className="w-full sm:w-[380px] pl-13 pr-3 py-3 h-[56px] placeholder-gray-300 border border-[#717387] rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[linear-gradient(to_bottom,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]"
                 />
               </div>
 
@@ -77,7 +79,9 @@ export default function HomePage() {
 
             <div className="flex gap-5 items-center">
               <Image src="/qr.png" alt="qr code" width={88} height={88} />
-              <span>Scan to Download App iOS & Android</span>
+              <span className="text-left">
+                Scan to Download App iOS & Android
+              </span>
             </div>
           </div>
           <Image
@@ -85,35 +89,37 @@ export default function HomePage() {
             alt="phone"
             width={506}
             height={600}
-            className="w-[500px] h-auto"
+            className="w-[500px] h-auto hidden sm:hidden lg:block"
           />
         </div>
 
-        <div className="grid grid-cols-2 gap-10 py-[180px] px-38 items-center">
-          <div className="grid grid-cols-2 grid-rows-2 gap-5">
-            <div className="flex flex-col justify-between border border-1 rounded-xl p-[24px] justify-between h-[193.5px] w-[278.5px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 py-[180px] px-10 sm:px-38 items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 grid-rows-2 gap-5">
+            <div className="flex flex-col justify-between border border-1 lg:w-full rounded-xl p-[24px] justify-between h-[193.5px] w-full sm:w-[278.5px] md:w-[220px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
               <p>All time volume</p>
               <p className="text-6xl">$5.4T</p>
             </div>
 
-            <div className="flex flex-col justify-between border border-1 rounded-xl p-[24px] justify-between h-[193.5px] w-[278.5px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
+            <div className="flex flex-col justify-between border border-1 lg:w-full rounded-xl p-[24px] justify-between h-[193.5px] w-full sm:w-[278.5px] md:w-[220px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
               <p>Users active</p>
               <p className="text-6xl">62M+</p>
             </div>
 
-            <div className="flex flex-col justify-between border border-1 rounded-xl p-[24px] justify-between h-[193.5px] w-[278.5px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
+            <div className="flex flex-col justify-between border border-1 lg:w-full rounded-xl p-[24px] justify-between h-[193.5px] w-full sm:w-[278.5px] md:w-[220px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
               <p>Supported countries</p>
               <p className="text-6xl">180</p>
             </div>
 
-            <div className="flex flex-col justify-between border border-1 rounded-xl p-[24px] justify-between h-[193.5px] w-[278.5px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
+            <div className="flex flex-col justify-between border border-1 lg:w-full rounded-xl p-[24px] justify-between h-[193.5px] w-full sm:w-[278.5px] md:w-[220px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)] hover:bg-[#4AFF96] hover:text-[#003717] duration-300">
               <p>24H trading volume (USD)</p>
               <p className="text-6xl">$2.6B</p>
             </div>
           </div>
 
-          <div className="flex flex-col gap-10 p-22">
-            <p className="text-5xl">Trusted by millions</p>
+          <div className="flex flex-col gap-10 p-10 sm:p-22 md:p-5 lg:items-center md:items-center">
+            <p className="text-2xl font-bold sm:text-5xl text-center xl:text-left md:text-4xl">
+              Trusted by millions
+            </p>
             <div className="flex flex-col gap-5">
               <div className="flex gap-4 bg-[#ffffff15] py-[8px] pl-[10px] pr-[20px] text-left rounded-md items-center">
                 <Image
@@ -161,12 +167,12 @@ export default function HomePage() {
           </div>
         </div>
 
-        <div className="bg-[url('/gradient.png')] pt-70 mt-[-180px] bg-no-repeat bg-cover flex flex-col gap-10 px-38">
-          <p className="text-5xl font-bold">
+        <div className="bg-[url('/gradient.png')] sm:pt-70 mt-[-50px] sm:mt-[-180px] lg:mt-[-300px] md:mt-[-50px] md:pt-0 lg:pt-40 text-center xl:text-left bg-no-repeat bg-cover flex flex-col gap-5 sm:gap-10 px-10 sm:px-38">
+          <p className="text-2xl sm:text-5xl font-bold md:text-4xl">
             Trade spot and margin. <br /> All in one powerful interface.
           </p>
 
-          <p className="text-gray-400">
+          <p className="text-gray-400 text-sm sm:text-md">
             Door7 products run on the powerful Door7 Protocol, the largest
             crypto marketplace.
           </p>
@@ -179,18 +185,20 @@ export default function HomePage() {
               height={1000}
               className="w-full border border-[#999999] rounded-md"
             />
-            <div className="absolute bottom-0 left-0 right-0 w-full h-[160px] backdrop-blur-xl bg-gradient-to-t from-black/80 to-transparent" />
+            <div className="absolute bottom-0 left-0 right-0 w-full h-[50px] sm:h-[160px] md:h-[60px] backdrop-blur-xl bg-gradient-to-t from-black/80 to-transparent" />
           </div>
         </div>
 
-        <div className="flex flex-col gap-5 items-center px-38">
-          <p className="text-5xl font-bold">Why Door7?</p>
-          <p className="text-gray-400">
+        <div className="flex flex-col gap-5 items-center px-10 sm:px-38 xl:text-left text-center">
+          <p className="text-2xl sm:text-5xl font-bold md:text-4xl">
+            Why Door7?
+          </p>
+          <p className="text-gray-400 sm:text-md text-sm">
             Simply and securely buy, sell, and manage hunreds of
             cryptocurrencies.
           </p>
-          <div className="grid grid-cols-3 grid-rows-2 gap-5 mt-10">
-            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 md:grid-cols-1 gap-5 mt-10">
+            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Send and receive crypto</p>
                 <p className="text-md text-gray-400">
@@ -206,7 +214,7 @@ export default function HomePage() {
               />
             </div>
 
-            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Stay updated with crypto news</p>
                 <p className="text-md text-gray-400">
@@ -222,7 +230,7 @@ export default function HomePage() {
               />
             </div>
 
-            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Loyalty program built for everyone</p>
                 <p className="text-md text-gray-400">
@@ -238,7 +246,7 @@ export default function HomePage() {
               />
             </div>
 
-            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+            <div className=" flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Deposit and withdraw at any time</p>
                 <p className="text-md text-gray-400">
@@ -254,7 +262,7 @@ export default function HomePage() {
               />
             </div>
 
-            <div className="group flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+            <div className="group flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Earn rewards for learning</p>
                 <p className="text-md text-gray-400">
@@ -270,7 +278,7 @@ export default function HomePage() {
               />
             </div>
 
-            <div className="group flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
+            <div className="group flex flex-col items-center justify-between gap-2 border border-1 rounded-xl pt-[24px] pb-[40px] px-[24px] h-[400px] w-[300px] lg:w-[350px] sm:w-[364px] bg-[linear-gradient(to_top,_rgba(255,255,255,0)_0%,_rgba(255,255,255,0.1)_100%)]">
               <div>
                 <p className="text-lg">Save your assets, earn bonuses</p>
                 <p className="text-md text-gray-400">
@@ -288,11 +296,11 @@ export default function HomePage() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-5 items-center p-38">
-          <p className="text-5xl font-bold text-center">
+        <div className="flex flex-col gap-5 items-center p-10 sm:p-38">
+          <p className="text-2xl sm:text-5xl font-bold text-center md:text-4xl">
             Explore crypto like Bitcoin, <br /> Ethereum, and Dogecoin
           </p>
-          <p className="text-gray-400">
+          <p className="text-gray-400 text-center">
             Simply and securely buy, sell, and manage hunreds of
             cryptocurrencies.
           </p>
@@ -323,7 +331,7 @@ export default function HomePage() {
             {loading ? (
               <p>Loading...</p>
             ) : (
-              <div className="grid grid-cols-6 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 2xl:grid-cols-6 gap-4">
                 {(activeTab === "tradeable"
                   ? coins.slice(0, 12)
                   : getTopGainers()
@@ -373,12 +381,11 @@ export default function HomePage() {
           </button>
         </div>
 
-        <div className="flex flex-col gap-5 items-center px-38">
-          <p className="text-5xl text-center text-white font-semibold">
-            “I&rsquo;ve tried several trading platforms, but <br /> this one
-            stands out. The intuitive UI, real- <br />
-            time analytics, and secure wallet make it <br /> my go-to for crypto
-            trading. Staking has <br /> never been this easy!”
+        <div className="flex flex-col gap-5 items-center p-10 sm:px-38">
+          <p className="text-2xl sm:text-5xl lg:text-4xl md:text-2xl text-center text-white font-semibold">
+            “I&rsquo;ve tried several trading platforms, but this one stands
+            out. The intuitive UI, real-time analytics, and secure wallet make
+            it my go-to for crypto trading. Staking has never been this easy!”
           </p>
 
           <div className="flex gap-4 items-center">

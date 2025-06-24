8153a613992ebcb359a05994c016632422ab7488
DanyFadhilah <fadilahdany@gmail.com>
2025-06-24 15:34:28 +0700

    Edit format image to svg page coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="bg-black p-2 h-screen">
-      <div className="bg-[url('/coming-soon.png')] h-[calc(100vh-50px)] w-full bg-no-repeat bg-cover bg-center rounded-2xl relative flex justify-center items-center px-10">
+      <div className="bg-[url('/bg-coming-soon.svg')] h-[calc(100vh-50px)] w-full bg-no-repeat bg-cover bg-center rounded-2xl relative flex justify-center items-center px-10">
         <div className="flex justify-center items-center z-10">
           <Image
-            src="/logo.png"
+            src="/logo.svg"
             alt="save"
             width={1000}
             height={1000}
@@ -16,7 +16,7 @@ export default function HomePage() {
           />
         </div>
 
-        <p className="absolute bottom-8 w-full text-[20px] text-center font-bold text-white text-xl z-10">
+        <p className="absolute bottom-8 w-full text-[20px] text-center font-semibold text-white z-10 tracking-[-2px]">
           Coming Soon
         </p>
       </div>
